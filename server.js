678634f763d8dc965e5eff7070f01a170c2ea524
afrique18 <vincentv1225@gmail.com>
2023-06-05 16:00:27 -0400

    Update server file

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const html_routes =require('./routes/html-routes')
-const api_routes = require('./routes/api-routes')
-const PORT = process.env.port || 3001;
+// Dependencies
+const express = require("express");
+
+// Sets up Express app 
 const app = express();
+const PORT = process.env.PORT || 3001
 
-app.use(express.urlencoded({ extended: false }));
+// handles data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use(express.static("./public/"));
 
+
+app.use("./routes/html-Rroutes");
+app.use("./routes/api-Routes");
+
+// Starts server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log("App listening on PORT " + PORT);
 });
